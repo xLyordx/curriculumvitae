a58aca349624f65dfc169c858d0857493c86d1f7
Carlos Pereira <carper010203@hotmail.com>
2021-01-15 20:04:32 -0500

    Initial Changes

--- carlospereiracv/src/app/app-routing.module.ts
+++ carlospereiracv/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { EducationComponent } from './education/education.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'Home', component: HomeComponent },
+  { path: 'Education', component: EducationComponent },
+  { path: '**', component: HomeComponent }  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- carlospereiracv/src/app/app.module.ts
+++ carlospereiracv/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { EducationComponent } from './education/education.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule,
